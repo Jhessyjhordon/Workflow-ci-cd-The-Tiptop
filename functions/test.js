83545b86ef21a1e806f1b4331cd0959caba9f007
aruseus98 <aruseus98@gmail.com>
2023-09-25 20:44:40 +0200

    Modification index.js pour ajouter des modules imports + utilisation de ces derniers dans test.js pour faire fonctionner le test

--- functions/test.js
+++ functions/test.js
@@ -1,13 +1,10 @@
 const assert = require('chai').assert;
 const sinon = require('sinon');
 const supertest = require('supertest');
-const express = require('express');
-const nodemailer = require('nodemailer'); // Ajoutez cette ligne pour importer nodemailer
-// const fs = require('fs');
 const mockFs = require('mock-fs');
+const app = require('./index.js');
 
 describe('submitContactForm', function () {
-    let app;
     let supertestApp;
     let mockTransporter;
 
@@ -22,19 +19,22 @@ describe('submitContactForm', function () {
     before(function () {
         // Configuration du système de fichiers virtuel avec le contenu du modèle d'e-mail
         mockFs({
-            './emailContactTemplate.hbs': testData.message, // Remplacez par le contenu réel de votre modèle d'e-mail
+            './emailContactTemplate.hbs': testData.message,
+            'node_modules': mockFs.load('node_modules/'),
         });
     });
 
     beforeEach(function () {
-        app = express();
         supertestApp = supertest(app);
 
-        mockTransporter = {
-            sendMail: sinon.stub(),
-        };
+        // Stubber le transporteur exporté de l'application
+        mockTransporter = app.transporter;
+        sinon.stub(mockTransporter, 'sendMail').resolves();
+    });
 
-        sinon.stub(nodemailer, 'createTransport').returns(mockTransporter); // Utilisez sinon.stub pour configurer le stub
+    afterEach(function() {
+        // Restaurer les comportements sinon stub après chaque test
+        sinon.restore();
     });
 
     after(function () {
@@ -43,10 +43,6 @@ describe('submitContactForm', function () {
     });
 
     it('should send an email when submitted', async function () {
-
-        // Vérifier que nodemailer.sendMail renvoie une promesse résolue
-        mockTransporter.sendMail.resolves();
-
         // Envoyer une demande HTTP POST à votre route Express avec les données de test
         const response = await supertestApp
             .post('/submit-contact-form')
@@ -58,16 +54,16 @@ describe('submitContactForm', function () {
         // Vérifier que la réponse JSON est correcte
         assert.equal(response.body.message, 'E-mail envoyé avec succès.');
 
+        console.log(mockTransporter.sendMail.getCall(0).args[0]);
+
         // Vérifier que nodemailer.sendMail a été appelé avec les bonnes données
         sinon.assert.calledWithMatch(mockTransporter.sendMail, {
             from: sinon.match(testData.email),
             to: sinon.match('jhessyismael@gmail.com'), // Remplacez par l'e-mail de destination attendu
             cc: sinon.match(testData.email),
             subject: sinon.match(testData.subject),
-            text: sinon.match(testData.message), // Utilisez le contenu réel du modèle d'e-mail ici
-            html: sinon.match(testData.message), // Utilisez le contenu réel du modèle d'e-mail ici
+            text: sinon.match.string, // cela vérifie simplement que le champ est une chaîne
+            html: sinon.match.string,
         });
     });
-
-
 });
