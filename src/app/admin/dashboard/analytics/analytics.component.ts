d0aad840f96b208818f469db5689c0ed5ac6252b
aruseus98 <aruseus98@gmail.com>
2023-10-30 00:16:31 +0100

    Ajout dashboard

---
+++ src/app/admin/dashboard/analytics/analytics.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-analytics',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './analytics.component.html',
+  styleUrls: ['./analytics.component.scss']
+})
+export class AnalyticsComponent {
+
+}
---
+++ src/app/admin/dashboard/user/user.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss']
+})
+export class UserComponent {
+
+}
