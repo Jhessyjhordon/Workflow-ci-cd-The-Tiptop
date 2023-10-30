156ff65176f2177b0e834c1e93ed7ff86bdfecfc
Jhessyjhordon <jhessyismael@gmail.com>
2023-10-30 21:51:14 +0100

    correction des quelque bug sur la page d'accueil et finalisation de la fonctionnalité de connexion

--- src/app/auth/login/login.component.spec.ts
+++ src/app/auth/login/login.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
-import { AuthService } from 'src/app/services/auth.service';
+import { AuthService } from 'src/app/services/auth/auth.service';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,20 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { BehaviorSubject, Observable, of, throwError } from 'rxjs';
+import { BehaviorSubject, Observable, catchError, map, of, throwError } from 'rxjs';
+import { AuthResponse } from 'src/app/models/auth-response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private isAuthenticated: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  private apiUrl = 'http://api.dev.dsp-archiwebo22b-ji-rw-ah.fr/user/login';
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private http: HttpClient) {
     // Vérifiez si un token est déjà présent lors de l'initialisation du service
     this.isAuthenticated.next(this.getToken() !== null);
   }
 
   setToken(token: string) {
-    localStorage.setItem('token', token);
+    localStorage.setItem('token', token); // enregistrez le jeton dans localStorage
     this.isAuthenticated.next(true); // Émet un signal que l'utilisateur est maintenant authentifié
   }
 
@@ -33,18 +36,30 @@ export class AuthService {
   }
 
   login({ email, password }: any): Observable<any> {
-    console.log("user try to log with user name : ", email, " and password : ", password);
-    
-    if (email === 'toto@gmail.com' && password === 'toto123') {
-      this.setToken('sdr-è_ujhbtyhubjgd"eYT_"ehbbcfjx');
-      return of({ name: 'Toto Dupond', email: 'toto@gmail.com' });
-    }
-    return throwError(new Error('Failed to login'));
+    const credentials = { email, password };
+    console.log("user try to log with user name : ", credentials.email, " and password : ", credentials.password);
+
+    return this.http.post<AuthResponse>(this.apiUrl, credentials).pipe(
+      map((response) => {
+        if (!response.error) {
+          // Si l'authentification réussit, enregistrez le jeton avec notre méthode setToken
+          this.setToken(response.jwt);
+          return response.message[0];
+        } else {
+          throw new Error(response.message[0]);
+        }
+      }),
+      catchError((error) => {
+        throw error;
+      })
+    );
+
+
   }
 
   signup({ firstname, lastname, phone, email, password }: any): Observable<any> {
     console.log("user try to signup with firstname : ", firstname, " lastname : ", lastname, " phone : ", phone, " email : ", email, " and password : ", password);
-    
+
     return throwError(new Error('Failed to signup'));
   }
 }
