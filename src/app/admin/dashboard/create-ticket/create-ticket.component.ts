57f127afdd422f4a54ccf7d2569f93aabfbcf2a9
vwez <desirv63@gmail.com>
2023-11-23 17:33:31 +0100

    add create user component

---
+++ src/app/admin/dashboard/create-ticket/create-ticket.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TicketService } from 'src/app/services/ticket/ticket.service';
+import { UserService } from 'src/app/services/user/user.service';
+import { UserCustomerShortcut } from 'src/app/models/user-customer-shortcut.model';
+import { FormsModule } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-create-ticket',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './create-ticket.component.html',
+  styleUrls: ['./create-ticket.component.scss']
+})
+
+export class CreateTicketComponent implements OnInit {
+  ticket: any = {}; // Créez une structure de données pour stocker les détails du ticket
+  users: UserCustomerShortcut[] = []; // Créez une structure de données pour stocker les utilisateurs
+  batches: any[] = []; // Créez une structure de données pour stocker les lots
+
+  constructor(private ticketService: TicketService, private userService: UserService) { }
+
+  ngOnInit() {
+    this.loadInitialData();
+  }
+
+  loadInitialData() {
+    this.userService.getShortcutCustomerDetails().subscribe(
+      (users: UserCustomerShortcut[]) => {
+        this.users = users;
+      },
+      error => {
+        console.error('Erreur lors du chargement des utilisateurs :', error);
+        // Gérez l'erreur selon vos besoins
+      }
+    );
+
+  }
+
+  onSubmit() {
+    // Ajoutez le traitement pour envoyer le ticket au backend
+    this.ticketService.createTicket(this.ticket).subscribe((response) => {
+      console.log('Ticket créé avec succès:', response);
+      // Ajoutez des actions supplémentaires si nécessaire
+    }, (error) => {
+      console.error('Erreur lors de la création du ticket:', error);
+      // Gérez les erreurs ici
+    });
+  }
+}
