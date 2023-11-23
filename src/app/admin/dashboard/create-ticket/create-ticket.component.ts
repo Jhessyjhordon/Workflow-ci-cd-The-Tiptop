516987b63f0f0b3e5e209372981f04f50b13ed84
vwez <desirv63@gmail.com>
2023-11-23 23:32:52 +0100

    call batch service and interface in create ticket logic

--- src/app/admin/dashboard/create-ticket/create-ticket.component.ts
+++ src/app/admin/dashboard/create-ticket/create-ticket.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TicketService } from 'src/app/services/ticket/ticket.service';
 import { UserService } from 'src/app/services/user/user.service';
+import { BatchesService } from 'src/app/services/batches/batches.service';
 import { UserCustomerShortcut } from 'src/app/models/user-customer-shortcut.model';
+import { ShortcutedBatch } from 'src/app/models/batch-shortcut.model';
 import { FormsModule } from '@angular/forms';
 
 
@@ -17,9 +19,11 @@ import { FormsModule } from '@angular/forms';
 export class CreateTicketComponent implements OnInit {
   ticket: any = {}; // Créez une structure de données pour stocker les détails du ticket
   users: UserCustomerShortcut[] = []; // Créez une structure de données pour stocker les utilisateurs
-  batches: any[] = []; // Créez une structure de données pour stocker les lots
+  batches: ShortcutedBatch[] = []; // Créez une structure de données pour stocker les lots
 
-  constructor(private ticketService: TicketService, private userService: UserService) { }
+  constructor(private ticketService: TicketService, 
+              private userService: UserService,
+              private batchService: BatchesService) { }
 
   ngOnInit() {
     this.loadInitialData();
@@ -29,6 +33,16 @@ export class CreateTicketComponent implements OnInit {
     this.userService.getShortcutCustomerDetails().subscribe(
       (users: UserCustomerShortcut[]) => {
         this.users = users;
+      },
+      error => {
+        console.error('Erreur lors du chargement des utilisateurs :', error);
+        // Gérez l'erreur selon vos besoins
+      }
+    );
+
+    this.batchService.getShortcutedBatchs().subscribe(
+      (batshs: ShortcutedBatch[]) => {
+        this.batches = batshs;
       },
       error => {
         console.error('Erreur lors du chargement des utilisateurs :', error);
