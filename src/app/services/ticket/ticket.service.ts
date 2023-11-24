565d62a1a5f253e09afbaf2efaf317cfcaa811f3
vwez <desirv63@gmail.com>
2023-11-24 02:06:51 +0100

    request the api for ticket creation createTicket

--- src/app/services/ticket/ticket.service.ts
+++ src/app/services/ticket/ticket.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, catchError, map, tap, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { TicketCreation } from 'src/app/models/ticket-creation.model';
+// import { UserService } from 'src/app/services/user/user.service';
+// import { UserCustomerShortcut } from 'src/app/models/user-customer-shortcut.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,21 +15,19 @@ export class TicketService {
 
   constructor(private http: HttpClient) { }
 
-  getUsers(): Observable<any[]> {
-    // Récupérer la liste des utilisateurs depuis le backend
-    const url = `${this.endpointUrl}/users`;
-    return this.http.get<any[]>(url);
-  }
-
-  getBatches(): Observable<any[]> {
-    // Récupérer la liste des lots depuis le backend
-    const url = `${this.endpointUrl}/batches`;
-    return this.http.get<any[]>(url);
-  }
 
   createTicket(ticket: any): Observable<any> {
     // Envoyer les données du nouveau ticket au backend
     const url = `${this.endpointUrl}/tickets`;
-    return this.http.post<any>(url, ticket);
+    return this.http.post<any>(url, ticket).pipe(
+      catchError((error) => {
+        if (error.status === 409) {
+          // Gérez l'erreur spécifique ici, par exemple en informant l'utilisateur
+          alert('Un conflit est survenu : ' + error.error.message);
+        }
+        // Vous pouvez aussi retransmettre l'erreur si vous voulez la gérer ailleurs
+        return throwError(() => new Error('Une erreur est survenue lors de la creation du tocket : ' + error.message));
+      })
+    );
   }
 }
