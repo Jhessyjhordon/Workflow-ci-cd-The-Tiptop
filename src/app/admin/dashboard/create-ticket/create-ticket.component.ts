d557aede3746bac706bfc55b0c0acd5685515c28
vwez <desirv63@gmail.com>
2023-11-24 02:01:57 +0100

    handdle ticket submition

--- src/app/admin/dashboard/create-ticket/create-ticket.component.ts
+++ src/app/admin/dashboard/create-ticket/create-ticket.component.ts
@@ -1,31 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { TicketService } from 'src/app/services/ticket/ticket.service';
 import { UserService } from 'src/app/services/user/user.service';
 import { BatchesService } from 'src/app/services/batches/batches.service';
 import { UserCustomerShortcut } from 'src/app/models/user-customer-shortcut.model';
 import { ShortcutedBatch } from 'src/app/models/batch-shortcut.model';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, FormGroup, Validators, FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-create-ticket',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterModule],
   templateUrl: './create-ticket.component.html',
   styleUrls: ['./create-ticket.component.scss']
 })
 
 export class CreateTicketComponent implements OnInit {
+  ticketForm!: FormGroup; 
   ticket: any = {}; // Créez une structure de données pour stocker les détails du ticket
   users: UserCustomerShortcut[] = []; // Créez une structure de données pour stocker les utilisateurs
   batches: ShortcutedBatch[] = []; // Créez une structure de données pour stocker les lots
 
   constructor(private ticketService: TicketService, 
               private userService: UserService,
-              private batchService: BatchesService) { }
+              private batchService: BatchesService,
+              private formBuilder: FormBuilder,) { }
 
   ngOnInit() {
+
+    this.ticketForm = this.formBuilder.group({
+      montantTicket: [null, [Validators.required, Validators.min(0)]],
+      userId: [null, Validators.required],
+      batchId: [null, Validators.required]
+    });
     this.loadInitialData();
   }
 
@@ -53,13 +62,20 @@ export class CreateTicketComponent implements OnInit {
   }
 
   onSubmit() {
-    // Ajoutez le traitement pour envoyer le ticket au backend
-    this.ticketService.createTicket(this.ticket).subscribe((response) => {
-      console.log('Ticket créé avec succès:', response);
-      // Ajoutez des actions supplémentaires si nécessaire
-    }, (error) => {
-      console.error('Erreur lors de la création du ticket:', error);
-      // Gérez les erreurs ici
-    });
+    if (this.ticketForm.valid) {
+      // Si le formulaire est valide, effectuez le traitement
+      const ticketData = this.ticketForm.value;
+      this.ticketService.createTicket(ticketData).subscribe(
+        (response) => {
+          console.log('Ticket créé avec succès:', response);
+        },
+        (error) => {
+          console.error('Erreur lors de la création du ticket:', error);
+        }
+      );
+    } else {
+      // Si le formulaire n'est pas valide, affichez un message ou effectuez d'autres actions nécessaires.
+      console.log('Le formulaire n\'est pas valide');
+    }
   }
 }
