a1d4ee471bd2e16f2c7028e2c797ae583a65a21c
vwez <desirv63@gmail.com>
2023-11-28 21:44:22 +0100

    fix angular json

--- src/app/user/dashboard/profile/profile.component.ts
+++ src/app/user/dashboard/profile/profile.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ToastrService } from 'ngx-toastr';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { UserService } from 'src/app/services/user/user.service';
@@ -20,7 +19,7 @@ export class ProfileComponent implements OnInit {
   
   userData!: UserCustomer | null;
 
-  constructor(private userService: UserService, private toastr: ToastrService) { }
+  constructor(private userService: UserService) { }
 
   // 
   ngOnInit() {
@@ -73,14 +72,30 @@ export class ProfileComponent implements OnInit {
     this.userService.deleteAccount().subscribe(
       () => {
         // Gérer le succès ici (par exemple, afficher un toast)
-        this.toastr.success('Compte supprimé avec succès', 'Succès');
+        // this.submissionResult = {
+        //   success: true,
+        //   message: "Compte supprimé avec succès"
+        //   // message: response.message[0],
+        // };
+        alert("Compte supprimé avec succès");
+
         // Rediriger ou effectuer d'autres actions nécessaires
       },
       (error) => {
         // Gérer les erreurs ici (par exemple, afficher un toast d'erreur)
-        this.toastr.error('Erreur lors de la suppression du compte', 'Erreur');
-        console.error(error);
+        // this.submissionResult = {
+        //   success: false,
+        //   message: "Erreur lors de la suppression du compte"
+        //   // message: response.message[0],
+        // };
+        alert('Erreur lors de la suppression du compte' + error);
       }
+
+      
     );
+
+    setTimeout(() => {
+      this.closeToast();
+    }, 5000);
   }
 }
