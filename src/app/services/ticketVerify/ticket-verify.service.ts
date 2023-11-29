d98d76906579090ad2b149196d211143f9bcfaff
Jhessyjhordon <jhessyismael@gmail.com>
2023-11-29 23:48:08 +0100

    affichage des gains sur la page recompences

--- src/app/services/ticketVerify/ticket-verify.service.ts
+++ src/app/services/ticketVerify/ticket-verify.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, catchError, map, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { AuthService } from '../auth/auth.service';
+import { AllBatch } from 'src/app/models/all-batch.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +12,10 @@ export class TicketVerifyService {
 
   private endpointUrl = environment.endpointUrl; // Endpoint de l'API 
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private authService: AuthService) { }
 
   verifyTicket(numTicket: number): Observable<any> {
-    console.log(numTicket); 
+    console.log(numTicket);
     return this.http.post(this.endpointUrl + '/ticket/verify/', { numTicket })
       .pipe(
         catchError((error) => {
@@ -27,8 +29,25 @@ export class TicketVerifyService {
       );
   }
 
-  patchTicketUserId(id: number, data: object): Observable<any> {
+  patchTicket(id: number, data: object): Observable<any> {
     const url = `${this.endpointUrl}/ticket/${id}`;
     return this.http.patch(url, data);
+  }
+ 
+  // récupère le lot en fonction de l'ID du user connecté
+  getBatchByTicketUserId(): Observable<any> {
+    const url = `${this.endpointUrl}/ticket/byuserid`;
+
+    // Obtenez le uderId à partir de votre service d'authentification
+    const userId = this.authService.getIdUser();
+
+    return this.http.post<{ data: AllBatch  }>(url, { userId })
+      .pipe(
+        map(response => response.data),
+        catchError(error => {
+          console.error('Une erreur s\'est produite lors de la récupération des données du lot:', error);
+          return throwError(error);
+        })
+      );
   }
 }
