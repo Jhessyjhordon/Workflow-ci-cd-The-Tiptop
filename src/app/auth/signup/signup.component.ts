79064008adf346827ff60f488a72a8004c40f983
Jhessyjhordon <jhessyismael@gmail.com>
2023-11-30 18:59:56 +0100

    I add a validator to block recent dates for the date field

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
-import {GoogleLoginButtonComponent} from '../../auth/google-login-button/google-login-button.component';
-import { FormGroup, FormBuilder, Validators, ReactiveFormsModule, ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
+import { GoogleLoginButtonComponent } from '../../auth/google-login-button/google-login-button.component';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule, ValidatorFn, AbstractControl, ValidationErrors, AsyncValidatorFn } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { Meta, Title } from '@angular/platform-browser';
 
@@ -13,8 +13,8 @@ import { Meta, Title } from '@angular/platform-browser';
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent implements OnInit  {
-  title= "S'inscrire | Thé Tiptop | Jeu concours";
+export class SignupComponent implements OnInit {
+  title = "S'inscrire | Thé Tiptop | Jeu concours";
   loginForm!: FormGroup;
   formSubmitted: boolean = false;
 
@@ -24,7 +24,7 @@ export class SignupComponent implements OnInit  {
 
   isLoggedAsAdmin: boolean = false; // True si on est connecté en tant qu'Admin
 
-  constructor(private auth: AuthService, private router: Router, private fb: FormBuilder, private titleService : Title, private metaService: Meta) {
+  constructor(private auth: AuthService, private router: Router, private fb: FormBuilder, private titleService: Title, private metaService: Meta) {
     this.loginForm = this.buildCommonForm();
     this.titleService.setTitle(this.title);
     this.addTag();
@@ -35,7 +35,7 @@ export class SignupComponent implements OnInit  {
   addTag() {
     this.metaService.addTag({ httpEquiv: 'Content-Type', content: 'text/html' }); // Indique aux agents et serveurs de prendre le contenu de cette page en tant que HTML
     this.metaService.addTag({ name: 'description', content: "S'inscrire à Thé Tiptop, site de jeu concours de thé pour les 10 ans et l'ouverture de la boutique à Nice" }); // Meta description de la page
-    this.metaService.addTag({ property: 'og-type', content: "Site web"}); /* Indique le type de l'objet */
+    this.metaService.addTag({ property: 'og-type', content: "Site web" }); /* Indique le type de l'objet */
     this.metaService.addTag({ name: 'robots', content: 'index,follow' }); // Permet au robot d'indexer la page
     this.metaService.addTag({ name: 'keywords', content: 'inscription jeu Nice' }); //Add keyword
     this.metaService.addTag({ property: 'og:title', content: "S'inscrire | Thé Tiptop | Jeu concours" }) // Titre pour l'encadré dans les recherches
@@ -77,9 +77,9 @@ export class SignupComponent implements OnInit  {
       password: ['', [Validators.required, Validators.pattern(passwordsRegex)]],
       confirmPassword: ['', [Validators.required, Validators.pattern(passwordsRegex)]],
       address: ['', [Validators.required, Validators.pattern(addressRegex)]],
-      birthDate: ['', [Validators.required]],
+      birthDate: ['', [Validators.required], [this.maxDateValidator()]],
       role: ['customer', [Validators.required, Validators.pattern(rolesRegex)]],
-      newsletter: [0] 
+      newsletter: [0]
     }, {
       validator: this.mustMatch('password', 'confirmPassword') // On gère ici la comparaison de pass et confirm password pour qu'ils soient à l'identique
     });
@@ -104,41 +104,41 @@ export class SignupComponent implements OnInit  {
       this.formSubmitted = false;
       const loginData = this.loginForm.value; // On créer une constante et on ajoute les valeurs du formulaire d'inscription dedans
 
-      console.log("~~~~~~~>", loginData );
-      
+      console.log("~~~~~~~>", loginData);
+
       // On fait appel à la méthode signup du Service AuthService pour effectuer l'inscription
-      this.auth.signup(loginData).subscribe(
-        (result) => {
-          this.submissionResult = {
-            success: true,
-            message: result.message,
-          };
-          this.loginForm.reset(); // Réinitialiser le formulaire après la soumission réussie
-          this.router.navigate(['']) // Redirige vers la home
-        },
-        (err: Error) => {
-          console.error("==============>>>>>>>>", err);
-          this.submissionResult = {
-            success: false,
-            message:
-              "Une erreur s'est produite lors de l'envoi du message. Veuillez réessayer plus tard.",
-          };
-        }
-      )
+      // this.auth.signup(loginData).subscribe(
+      //   (result) => {
+      //     this.submissionResult = {
+      //       success: true,
+      //       message: result.message,
+      //     };
+      //     this.loginForm.reset(); // Réinitialiser le formulaire après la soumission réussie
+      //     this.router.navigate(['']) // Redirige vers la home
+      //   },
+      //   (err: Error) => {
+      //     console.error("==============>>>>>>>>", err);
+      //     this.submissionResult = {
+      //       success: false,
+      //       message:
+      //         "Une erreur s'est produite lors de l'envoi du message. Veuillez réessayer plus tard.",
+      //     };
+      //   }
+      // )
     }
   }
 
   // Méthode qui permet de vérifier si 2 champs ont la même valeur lors de la saisie
-  mustMatch(controlName: string, matchingControlName: string){
+  mustMatch(controlName: string, matchingControlName: string) {
     return (formGroup: FormGroup) => {
       const control = formGroup.controls[controlName];
       const matchingControl = formGroup.controls[matchingControlName];
-  
+
       if (matchingControl.errors && !matchingControl.errors['mustMatch']) {
         // retourner si un autre validateur a déjà trouvé une erreur sur le MatchingControl
         return;
       }
-  
+
       // définir une erreur sur matchingControl si la validation échoue
       if (control.value !== matchingControl.value) {
         matchingControl.setErrors({ mustMatch: true });
@@ -152,5 +152,41 @@ export class SignupComponent implements OnInit  {
     // Redirige l'utilisateur vers Google pour l'authentification
     this.auth.redirectToGoogleAuth();
   }
+
+  // Fonction qui limite le choix des dates à un an avant la date actuelle pour l'affichage des dates dans l'UI du champ date
+  getMaxDate(): string {
+    // Obtenir la date actuelle
+    const currentDate = new Date();
+
+    // Règle : la date maximale est la date actuelle moins un an
+    currentDate.setFullYear(currentDate.getFullYear() - 1);
+
+    // Formater la date au format ISO (YYYY-MM-DD) pour l'attribut max
+    const maxDate = currentDate.toISOString().split('T')[0];
+
+    return maxDate;
+  }
+
+
+  // Validator qui limite le choix des dates à un an avant la date actuelle et trigger le message d'erreur en cas de date trop recent
+  maxDateValidator(): AsyncValidatorFn {
+    return (control: AbstractControl): Promise<ValidationErrors | null> => {
+      return new Promise((resolve) => {
+        const selectedDate = new Date(control.value);
+        const currentDate = new Date();
+
+        // Règle : la date maximale est la date actuelle moins un an
+        currentDate.setFullYear(currentDate.getFullYear() - 1);
+
+        // Comparer la date sélectionnée avec la date actuelle
+        if (selectedDate > currentDate) {
+          resolve({ 'maxDate': true });
+        } else {
+          resolve(null);
+        }
+      });
+    };
+  }
+
 
 }
