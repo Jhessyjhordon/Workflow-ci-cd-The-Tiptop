217bc2773c9435aca517c05cb852a7941007e63d
aruseus98 <aruseus98@gmail.com>
2023-12-04 14:53:30 +0100

    Correction, remove environments

--- src/app/services/batches/batches.service.ts
+++ src/app/services/batches/batches.service.ts
@@ -2,7 +2,6 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, catchError, map, throwError } from 'rxjs';
 import { AuthService } from '../auth/auth.service';
-import { environment } from 'src/environments/environment';
 import { ShortcutedBatch } from 'src/app/models/batch-shortcut.model';
 
 @Injectable({
