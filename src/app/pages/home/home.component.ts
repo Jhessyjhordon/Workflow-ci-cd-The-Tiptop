55b4958c71a4186e5e936b30f9a4e20557f026f1
aruseus98 <aruseus98@gmail.com>
2023-12-04 18:21:03 +0100

    Test canonical link home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -20,12 +20,14 @@ export class HomeComponent {
 
   // Définition des différentes balises pour le SEO
   addTag() {
+    const url = 'https://dsp-archiwebo22b-ji-rw-ah.fr/';
     this.metaService.addTag({ httpEquiv: 'Content-Type', content: 'text/html' }); // Indique aux agents et serveurs de prendre le contenu de cette page en tant que HTML
     this.metaService.addTag({ name: 'description', content: "Thé Tiptop, site de jeu concours de thé pour les 10 ans et l'ouverture de la boutique à Nice" }); // Meta description de la page
     this.metaService.addTag({ property: 'og-type', content: "Site web"}); /* Indique le type de l'objet */
     this.metaService.addTag({ name: 'robots', content: 'index,follow' }); // Permet au robot d'indexer la page
     this.metaService.addTag({ name: 'keywords', content: 'jeu concours Nice' }); //Add keyword
     this.metaService.addTag({ property: 'og:title', content: "Thé Tiptop | Jeu concours" }) // Titre pour l'encadré dans les recherches
+    this.metaService.addTag({ name: 'canonical', href: url });
   }
 
   images: string[] = [
